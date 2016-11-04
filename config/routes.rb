9118779ff228efdd83ee6573bd8a922829799bb6
TIYZAP <ironyardzap@gmail.com>
2016-11-04 16:00:41 -0400

    Get to Post

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ Rails.application.routes.draw do
     resources :shouts
     post '/log_in' => 'users#log_in'
     post '/signup' => 'users#create'
-    get '/all' => 'users#index'
-    get '/follow' => 'users#follow_user'
-    get '/unfollow' => 'users#unfollow_user'
-    get '/allfollowers' => 'users#all_followers'
+    post '/all' => 'users#index'
+    post '/follow' => 'users#follow_user'
+    post '/unfollow' => 'users#unfollow_user'
+    post '/allfollowers' => 'users#all_followers'
   end
 
 end
